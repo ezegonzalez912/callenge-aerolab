c6c2b52f8159fa660798b800d74a12c49db736fa
unknown <ezegonzalez912@gmail.com>
2021-09-11 19:16:21 -0300

    fix router

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -8,9 +8,9 @@ export const AppRouter = () => {
         <Router>
             <Navbar />
             <Switch>
-                <Route exact path="/user" component={UserScreen}/>
-                <Route exact path="/" component={HomeScreen}/>
-                <Redirect to="/"/>
+                <Route exact path="callenge-aerolab/user" component={UserScreen}/>
+                <Route exact path="/callenge-aerolab/" component={HomeScreen}/>
+                <Redirect to="/callenge-aerolab/"/>
             </Switch>
         </Router>
     )
